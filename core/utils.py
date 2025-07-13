648b3ba92487147056611b7d54aa61881aec54e8
Senimtra <senimtra@hotmail.com>
2025-07-13 13:37:03 +0200

    add cat investor: tilda coincroft

--- core/utils.py
+++ core/utils.py
@@ -25,6 +25,10 @@ os.environ['AURELIA_SYMBOLS'] = os.getenv('AURELIA_SYMBOLS')
 os.environ['AURELIA_SHARES'] = os.getenv('AURELIA_SHARES')
 os.environ['AURELIA_PRICES'] = os.getenv('AURELIA_PRICES')
 
+os.environ['TILDA_SYMBOLS'] = os.getenv('TILDA_SYMBOLS')
+os.environ['TILDA_SHARES'] = os.getenv('TILDA_SHARES')
+os.environ['TILDA_PRICES'] = os.getenv('TILDA_PRICES')
+
 
 # Extract Mortimer portfolio symbols, shares, prices
 SYMBOLS_MORTIMER = os.environ['MORTIMER_SYMBOLS'].split()
@@ -46,10 +50,15 @@ SYMBOLS_AURELIA = os.environ['AURELIA_SYMBOLS'].split()
 SHARES_AURELIA = os.environ['AURELIA_SHARES'].split()
 PRICES_AURELIA = os.environ['AURELIA_PRICES'].split()
 
+# Extract Tilda Coincroft portfolio symbols, shares, prices
+SYMBOLS_TILDA = os.environ['TILDA_SYMBOLS'].split()
+SHARES_TILDA = os.environ['TILDA_SHARES'].split()
+PRICES_TILDA = os.environ['TILDA_PRICES'].split()
+
 
 # Check today's stock mood
 def check_stockMood():
-    all_symbols = list(set(SYMBOLS_MORTIMER + SYMBOLS_CECIL + SYMBOLS_GWENDOLYN + SYMBOLS_AURELIA))  # unique symbols
+    all_symbols = list(set(SYMBOLS_MORTIMER + SYMBOLS_CECIL + SYMBOLS_GWENDOLYN + SYMBOLS_AURELIA + SYMBOLS_TILDA))  # unique symbols
     up = down = flat = 0
     # Check symbols
     for symbol in all_symbols:
@@ -124,6 +133,17 @@ def check_aurelia_goldwhisker():
     # Calculate invest_current
     prices_current = [get_prices_yfinance(symbol) for symbol in SYMBOLS_AURELIA]
     invest_current = sum([float(share) * price for share, price in zip(SHARES_AURELIA, prices_current)])
+    # Calculate performance
+    performance = round(((invest_current * 100) / invest_start) - 100, 2)
+    return (performance, 'simulated')
+
+# Check Tilda Coincroft pawfolio
+def check_tilda_coincroft():
+    # Calculate invest_start
+    invest_start = sum([float(share) * float(price) for share, price in zip(SHARES_TILDA, PRICES_TILDA)])
+    # Calculate invest_current
+    prices_current = [get_prices_yfinance(symbol) for symbol in SYMBOLS_TILDA]
+    invest_current = sum([float(share) * price for share, price in zip(SHARES_TILDA, prices_current)])
     # Calculate performance
     performance = round(((invest_current * 100) / invest_start) - 100, 2)
     return (performance, 'simulated')
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-from .utils import check_mortimer, check_cecil, check_gwendolyn, check_aurelia_goldwhisker, check_stockMood
+from .utils import check_mortimer, check_cecil, check_gwendolyn, check_aurelia_goldwhisker, check_tilda_coincroft, check_stockMood
 
 
 @ensure_csrf_cookie
@@ -15,13 +15,15 @@ def index(request):
     performance_cecil, status_cecil = check_cecil()
     performance_gwendolyn, status_gwendolyn = check_gwendolyn()
     performance_aurelia, status_aurelia = check_aurelia_goldwhisker()
+    performance_tilda, status_tilda = check_tilda_coincroft()
     # Pawfolio checks
     pawfolios = [
         {'name': 'Mortimer', 'perf_value': performance_mortimer,'status': status_mortimer},
         {'name': 'Cecil', 'perf_value': performance_cecil,'status': status_cecil},
-        {'name': 'Gwendolyn', 'perf_value': performance_gwendolyn,'status': status_gwendolyn},
+        {'name': 'Gwendolyn', 'perf_value': 0,'status': 'simulated'},
         {'name': 'Aunt Elinor', 'perf_value': 0,'status': 'simulated'},
-        {'name': 'Aurelia Goldwhisker', 'perf_value': performance_aurelia, 'status': status_aurelia}
+        {'name': 'Aurelia Goldwhisker', 'perf_value': performance_aurelia, 'status': status_aurelia},
+        {'name': 'Tilda Coincroft', 'perf_value': performance_tilda, 'status': status_tilda}
     ]
     # Set up donations
     donations = range(17, 0, -1)
@@ -38,9 +40,10 @@ def get_pawformances(request):
     pawformances = {
         'Mortimer': check_mortimer(),
         'Cecil': check_cecil(),
-        'Gwendolyn': check_gwendolyn(),
+        'Gwendolyn': (0, 'simulated'),
         'Aunt Elinor': (0, 'simulated'),
-        'Aurelia Goldwhisker': check_aurelia_goldwhisker()
+        'Aurelia Goldwhisker': check_aurelia_goldwhisker(),
+        'Tilda Coincroft': check_tilda_coincroft()
     }
     return JsonResponse(pawformances)
 
